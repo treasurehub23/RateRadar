e7f72226dabc0c45dbd281559fac9470dd43d563
treasurehub23 <okegbolatreasure@gmail.com>
2026-09-23 11:24:45 +0100

    reliability status

--- ML/classes.py
+++ ML/classes.py
@@ -1,4 +1,5 @@
-from pydantic import Field, BaseModel
+from pydantic import Field, BaseModel, ValidationError
+from typing import Literal
 class ExtractFeeInput(BaseModel):
     text: str
     source: str
@@ -17,7 +18,20 @@ class AnomalyInput(BaseModel):
     recent_rates: list[float]
 
 class AnomalyResult(BaseModel):
-    isAnomaly: bool
+    isAnomaly: bool = False
     median: float | None
     percentage_deviation: float | None
-    insufficient_history: bool    
+    insufficient_history: bool   = True  
+
+class MerchantInput(BaseModel):
+    completion_rate: float
+    order_count: int
+    average_release_time: float # Still need to know the unit
+    recent_reviews: list[str]
+
+Status = Literal["Low", "Medium", "High"]
+class MerchantResult(BaseModel):
+    reliability_status: Status |None
+    reason: str |None 
+    low_order_count: bool = False
+    error: str | None = None
